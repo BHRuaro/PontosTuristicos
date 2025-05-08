bd1ac88129d8bbf8587f90fbeb9d89c7a809c4c6
Bruno Ruaro <brunoruaro@alunos.utfpr.edu.br>
2025-05-07 22:26:18 -0300

    segundo commit

---
+++ app/src/main/java/br/edu/pontosturisticos/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.edu.pontosturisticos
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun btMapOnClick(view: View) {
+        val i = Intent(this, MapsActivity::class.java)
+        startActivity( i )
+    }
+}
